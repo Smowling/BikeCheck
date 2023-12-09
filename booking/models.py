bc0eb8c5f8d847207fa46b28404a686d23872605
Daniel Smółka <smowling@gmail.com>
2023-12-09 12:12:31 +0100

    models

--- booking/models.py
+++ booking/models.py
@@ -10,24 +10,9 @@ class Adress(models.Model):
     def __str__(self):
         return f'{self.city} {self.postcode}, {self.street} {self.street_number}'
 
-class Contact(models.Model):
-    phone = models.CharField(max_length=50)
-    email = models.EmailField()
-
-    def __str__(self):
-        return f"phone: {self.phone}, email: {self.email}"
 
    
 class User(User):
-    contact = models.OneToOneField(
-        Contact, on_delete = models.CASCADE,
-        null = True
-    )
-    adress = models.ForeignKey(
-        Adress,
-        on_delete = models.CASCADE,
-        null = True
-    )
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
@@ -35,16 +20,17 @@ class User(User):
 class Store(models.Model):
     name = models.CharField(max_length=200)
     details = models.CharField(max_length=1000)
-    adress = models.ForeignKey(
-        Adress,
-        on_delete = models.CASCADE,
-        null = True
-    )
-    contact = models.ForeignKey(
-        Contact,
-        on_delete=models.CASCADE,
-        null = True
-    )
 
     def __str__(self):
         return f'{self.name}'
+
+
+
+class Contact(models.Model):
+    phone = models.CharField(max_length=50)
+    email = models.EmailField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"phone: {self.phone}, email: {self.email}"
--- booking/tests.py
+++ booking/tests.py
@@ -7,68 +7,78 @@ from .models import User, Store, Adress, Contact
 class ContactModelTest(TestCase):
     def test_contact_model_exists(self):
         contact = Contact.objects.count()
-
         self.assertEqual(contact, 0)
 
     def test_contact_has_string_representation(self):
         contact = Contact.objects.create(phone="123123123", email="asd@asd.pl")
-
         self.assertEqual(str(contact), "phone: 123123123, email: asd@asd.pl")
 
 
 class UserModelTest(TestCase):
     def test_user_model_exists(self):
         users = User.objects.count()
-
         self.assertEqual(users, 0)
 
     def test_user_has_string_representation(self):
         user = User.objects.create(username="test", first_name="test", last_name="test")
-
         self.assertEqual(str(user), "test test")
 
+    def test_user_contact_exists(self):
+        user = User.objects.create(username="test", first_name="test", last_name="test")
+        contact = Contact.objects.create(phone="123123123", email="user@user.pl", user=user)
+        contact = Contact.objects.get(id=user.id)
+        self.assertTrue(contact is not None)
+
+    def test_user_delete_contact_cascades(self):
+        user = User.objects.create(username="test", first_name="test", last_name="test")
+        contact = Contact.objects.create(phone="123123123", email="user@user.pl", user=user)
+        user.delete()
+        contact = Contact.objects.all().count()
+        self.assertEqual(contact, 0)
+        
 
 class StoreModelTest(TestCase):
     def test_store_model_exists(self):
         store = Store.objects.count()
-
         self.assertEqual(store, 0)
 
 
     def test_store_model_has_string_representation(self):
         store = Store.objects.create(name="store")
-
         self.assertEqual(str(store), "store")
 
 
     def test_store_contact_exists(self):
-        store = Store.objects.create(name="test", details="test details", contact=Contact.objects.create(phone="123123123", email="test@store.pl"))
+        store = Store.objects.create(name="test", details="test details")
+        contact = Contact.objects.create(phone="123123123", email="test@asd.pl", store=store)
         contact = Contact.objects.get(id=store.id)
-
         self.assertTrue(contact is not None)
 
     def test_store_delete_contact_cascades(self):
-        store = Store.objects.create(name="test", details="test details", contact=Contact.objects.create(phone="123123123", email="test@store.pl"))
-        store_id = store.id
-        store.contact.delete()
-        # contact = Contact.objects.filter(id=store_id)
+        store = Store.objects.create(name="test", details="test details")
+        contact = Contact.objects.create(phone="123123123", email="test@asd.pl", store=store)
+        contact2 = Contact.objects.create(phone="123123123", email="test@asd.pl", store=store)
 
-        store = Store.objects.filter(id=store_id)
-        contact = Contact.objects.filter(id=store_id)
-        self.assertEqual(str(store), 1)
+        self.assertTrue(store is not None)
+        count = Contact.objects.all().count()
+        self.assertEqual(count, 2)
+        store.delete()
+        store = Store.objects.all().count()
+        contact = Contact.objects.all().count()
+        self.assertEqual(store, 0)
+        self.assertEqual(contact, 0)
+        
 
 
 class AdressModelTest(TestCase):
     def test_adress_model_exists(self):
         adress = Adress.objects.count()
-
         self.assertEqual(adress, 0)
 
 
 class BookingIndexPageTest(TestCase):
     def test_booking_index_page_returns_200_response(self):
         response = self.client.get('/')
-
         self.assertTemplateUsed(response, 'booking/index.html')
         self.assertEqual(response.status_code, 200)
         
@@ -83,13 +93,11 @@ class StoreDetailsPageTest(TestCase):
 
     def test_store_details_page_returns_200_response(self):
         response = self.client.get(f'/store/{self.store.name}/')
-
         self.assertTemplateUsed(response, 'booking/details.html')
         self.assertEqual(response.status_code, 200)
 
 
     def test_store_details_page_has_store_details(self):
         response = self.client.get(f'/store/{self.store.name}/')
-
         self.assertContains(response, self.store.name)
         self.assertContains(response, self.store.details)
