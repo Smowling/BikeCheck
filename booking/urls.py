22eea33740180f3a69d011a413ca3444b0668aed
Daniel Smółka <smowling@gmail.com>
2023-12-12 18:58:34 +0100

    views

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path ('', views.index, name='index'),
-    path (f'store/<str:store_name>/', views.details, name='details')
+    path (f'store/<str:store_name>/', views.details, name='details'),
+    path (f'login/', views.login, name="login"),
+    path (f'register/', views.register, name="register"),
     
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+import json
 
-from .models import Store
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Store, User, Bike
 
 def index(request):
     return render(request, 'booking/index.html')
@@ -9,3 +17,55 @@ def index(request):
 def details(request, store_name):
     store = Store.objects.get(name=store_name)
     return render(request, 'booking/details.html', {'store_name': store.name, 'store': store})
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def login(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "booking/login.html", {
+                "message": "Invalid email and/or password."
+            })
+    else:
+        return render(request, "booking/login.html")
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        username = request.POST["username"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "booking/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "booking/register.html", {
+                "message": "Email address already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "booking/register.html")
